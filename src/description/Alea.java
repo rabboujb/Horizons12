21d9c36a97d8c09095fdfc6bccb48ccfc7ceb559
mickael <mickael.spilmont.fr@gmail.com>
2018-05-18 19:17:46 +0200

    Ajout des getter dans Tache ainsi que de la Javadoc sur Alea

--- src/description/Alea.java
+++ src/description/Alea.java
@@ -1,5 +1,14 @@
 package description;
 
+/**
+ * Classe représantant un Alea avec toute les information necessaire pour l'appliquer :
+ *      Le type
+ *      La couleur
+ *      la gravité
+ *      le nom
+ *
+ * @author Equipe Horizon12
+ */
 public class Alea {
 
     private final TypeAlea TYPE;
@@ -14,22 +23,42 @@ public class Alea {
         this.NOM = nom;
     }
 
+    /**
+     * Retourne le type de la tache
+     * @return TYPE
+     */
     public TypeAlea getTYPE() {
         return TYPE;
     }
 
+    /**
+     * Retourne la couleur de la tache
+     * @return COULEUR
+     */
     public Couleur getCOULEUR() {
         return COULEUR;
     }
 
+    /**
+     * Retourne la gravité de la tache
+     * @return GRAVITE
+     */
     public int getGRAVITE() {
         return GRAVITE;
     }
 
+    /**
+     * Retourne le nom de la tache
+     * @return NOM
+     */
     public String getNOM() {
         return NOM;
     }
 
+    /**
+     * Retourne une description de la tache et de ses attributs
+     * @return description
+     */
     public String toString() {
         StringBuilder chaine = new StringBuilder();
 
--- src/description/Tache.java
+++ src/description/Tache.java
@@ -1,12 +1,17 @@
 package description;
 
+/**
+ * Classe qui représente une tache
+ *
+ * @author Equipe Horizon12
+ */
 public class Tache {
 
     private final int ID;
     private final String NOM;
     private final int COUT;
     private final int SEMAINES;
-    private final int SEMAINES_REEL;
+    private final int SEMAINES_MAX;
 
     private final Alea ROUGE;
     private final Alea JAUNE;
@@ -15,22 +20,14 @@ public class Tache {
     private final int PREDECESSEUR;
     private final int SUCCESSEUR;
 
-    public Tache(int id,
-                 String nom,
-                 int cout,
-                 int semaines,
-//                 int semainesReel,
-                 Alea rouge,
-                 Alea jaune,
-                 Alea vert,
-                 int predecesseur,
-                 int successeur) {
+    public Tache(int id, String nom, int cout, int semaines, int semaineMax, Alea rouge, Alea jaune, Alea vert,
+                 int predecesseur, int successeur) {
 
         this.ID = id;
         this.NOM = nom;
         this.COUT = cout;
         this.SEMAINES = semaines;
-//        this.SEMAINES_REEL = semainesReel;
+        this.SEMAINES_MAX = semaineMax;
         this.ROUGE = rouge;
         this.JAUNE = jaune;
         this.VERT = vert;
@@ -38,6 +35,77 @@ public class Tache {
         this.SUCCESSEUR = successeur;
     }
 
+    /**
+     * Retourne l'Alea corespondant à la couleur passée en paramètre
+     * @param couleur
+     * @return un Alea
+     */
+    public Alea getAlea(Couleur couleur) {
+        if (couleur.equals(ROUGE)) {
+            return ROUGE;
+        }
+        else if (couleur.equals(JAUNE)) {
+            return JAUNE;
+        }
+        return VERT;
+    }
+
+    /**
+     * Retourne l'Id de la tache
+     * @return ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    public String getNOM() {
+        return NOM;
+    }
+
+    /**
+     * Retourne le coute d'acceleration de la tache
+     * @return COUT
+     */
+    public int getCOUT() {
+        return COUT;
+    }
+
+    /**
+     * Retourne le nombre de semaine par défaut de la tache
+     * @return SEMAINES
+     */
+    public int getSEMAINES() {
+        return SEMAINES;
+    }
+
+    /**
+     * Retourne le nombre de semaines maximum que peut durer la tache
+     * @return SEMAINES_MAX
+     */
+    public int getSEMAINES_MAX() {
+        return SEMAINES_MAX;
+    }
+
+    /**
+     * Retourne l'Id de la tache précédente
+     * @return PREDECESSEUR
+     */
+    public int getPREDECESSEUR() {
+        return PREDECESSEUR;
+    }
+
+    /**
+     * Retourne la tache suivante
+     * @return SUCCESSEUR
+     */
+    public int getSUCCESSEUR() {
+        return SUCCESSEUR;
+    }
+
+    /**
+     * Retourne une description de la tache et de ses aléas (en se servant du toString de ceux-ci)
+     * @return Description de la tache
+     */
     public String toString() {
         StringBuilder chaine = new StringBuilder();
 
@@ -47,5 +115,7 @@ public class Tache {
                 .append("\n").append(ROUGE)
                 .append("\n").append(JAUNE)
                 .append("\n").append(VERT);
+
+        return chaine.toString();
     }
 }
