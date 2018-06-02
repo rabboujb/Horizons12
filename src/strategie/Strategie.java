89f9851eb268e5de3d5ccfc7854d46be3b5181a2
bilal <b.rabbouj@gmail.com>
2018-06-02 20:36:27 +0200

    Mise à jour - Modification de l'interface Strategie

--- src/strategie/Strategie.java
+++ src/strategie/Strategie.java
@@ -3,9 +3,11 @@
  */
 
 package strategie;
+import description.Couleur;
 import partie.VueJoueur;
 
 public interface Strategie {
 	void jouerEtape(VueJoueur vue);
 	void jouerJalon(VueJoueur vue);
+	Couleur getTirage(int id);
 }
