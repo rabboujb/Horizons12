59eb76d3a037e86bf74091472683bc3fd15c2509
Abdoulaye Diallo <tadricardo92@gmail.com>
2018-06-13 09:16:27 +0200

    Mise à jour de l'affichage du pert

--- src/menu/MenuPartie.java
+++ src/menu/MenuPartie.java
@@ -2,6 +2,7 @@ package menu;
 import description.*;
 import partie.VueJoueur;
 import java.util.Scanner;
+import partie.Realisation;
 
 /**
  *
@@ -18,15 +19,13 @@ public class MenuPartie {
 		this.d = description;
 	}
 
-	public void consulterPert() {
+	
+        public void consulterPert() {
+            
         for(int i=1;i<d.getPlateau().size()+1;i++) {
-        	Tache t = d.getTacheById(i);
+            Realisation r = new Realisation(d.getTacheById(i));    
+            System.out.println(r);
 
-            System.out.println("TÂCHE "+t.getID());
-            System.out.println();
-            System.out.println(t);
-            System.out.println("===");
-            System.out.println();
         }
 	}
 
