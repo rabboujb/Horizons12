e893e2a68845b306e870caa07a0da63aa734a077
perrot <perrot@perrot-EasyNote-TSX62HR>
2018-06-17 15:41:57 +0200

    Création tests unit class TourFinal

---
+++ src/testsUnitDescription/TourFinalTest.java
@@ -0,0 +1,54 @@
+package testsUnitDescription;
+
+import static description.TypeTour.ALEA;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import description.TourAlea;
+import description.TourFinal;
+
+/**
+ * @author Ismérie
+ */
+
+
+public class TourFinalTest {
+	
+	private final TourFinal exampleTourFinal;
+	private TourFinal autreTourFinal;
+	
+	public TourFinalTest() {
+		this.exampleTourFinal = new TourFinal();
+		this.autreTourFinal = new TourFinal();
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+		System.out.println("Début du test de TourFinal");
+		System.out.println("Tour de référence: " + new TourFinal());
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+		System.out.println("Fin du test de TourFinal");
+	}
+	
+	@Before
+	public void setUp() {
+		this.autreTourFinal = new TourFinal();
+	}
+	
+	@After
+	public void tearDown() {
+		autreTourFinal = null;
+	}
+	
+	
+	
+
+}
