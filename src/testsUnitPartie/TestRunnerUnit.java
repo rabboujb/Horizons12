5fab198ab7861c6b820a840bf9ec266c95af3e20
perrot <perrot@perrot-EasyNote-TSX62HR>
2018-06-17 15:42:48 +0200

    mise en place du testRunnerUnit pour package Partie

---
+++ src/testsUnitPartie/TestRunnerUnit.java
@@ -0,0 +1,27 @@
+package testsUnitPartie;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import testsUnitDescription.TestsUnitSuite;
+
+/**
+ * @author Ismérie
+ */
+
+public class TestRunnerUnit {
+
+	public static void main(String[] args) {
+	      Result result = JUnitCore.runClasses(TestsUnitSuite.class);
+	  	
+	      for (Failure failure : result.getFailures()) {
+	         System.out.println(failure.toString());
+	      }
+			
+	      System.out.println(result.wasSuccessful());
+	   }
+
+	
+
+}
